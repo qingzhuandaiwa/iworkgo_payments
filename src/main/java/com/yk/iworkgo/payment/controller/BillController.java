438d6d6f6518b908467916405f1dfcf957a796a1
wade_gj <guoliqingchn@gmail.com>
2019-01-11 11:00:52 +0800

    ss

--- src/main/java/com/yk/iworkgo/payment/controller/BillController.java
+++ src/main/java/com/yk/iworkgo/payment/controller/BillController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import com.yk.iworkgo.common.BaseController;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -173,9 +174,9 @@ public class BillController extends BaseController {
             //test by guojing start
 //            if (data != null && data.getRows() != null){
 //                int i = 1;
-//                for (Iterator<Bill> iterator = data.getRows().iterator();iterator.hasNext();){
+//                for (Iterator<Bill> iterator = data.getRows().iterator(); iterator.hasNext();){
 //                    Bill bill = iterator.next();
-//                    if (i >= 5){
+//                    if (i >= 3){
 //                        iterator.remove();
 //                    }
 //                    i++;
